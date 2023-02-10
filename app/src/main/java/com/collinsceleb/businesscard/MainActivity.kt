fde96add1fd8074d0f25b7288ac5a719f52981f8
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2023-02-10 22:34:11 +0100

    Initialize project with texts and logo

--- app/src/main/java/com/collinsceleb/businesscard/MainActivity.kt
+++ app/src/main/java/com/collinsceleb/businesscard/MainActivity.kt
@@ -3,13 +3,23 @@ package com.collinsceleb.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.collinsceleb.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BusinessCard()
                 }
             }
         }
@@ -30,14 +40,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BusinessCard() {
+    val image = painterResource(id = R.drawable.android_logo)
+    Surface(color = Color.DarkGray) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Box {
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .width(100.dp)
+                        .height(100.dp)
+                )
+            }
+            Text(
+                stringResource(R.string.business_owner),
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Serif,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp))
+            Text(
+                stringResource(R.string.designation),
+                color = Color.Magenta,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Serif,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        BusinessCard()
     }
 }
